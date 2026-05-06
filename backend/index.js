4451cff4ec87f7d674774b083bd6831574a1733c
Asfiya Rudbar <asfiyarudbar@gmail.com>
2026-05-06 17:50:59 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,30 @@
-// index.js
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
-// Import your routes
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 
-// Connect to MongoDB
 connectDB();
 
 const app = express();
 
 app.use("/images", express.static("images"));
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
-// Root route for testing
 app.get("/", (req, res) => {
   res.send(" Coffee House Backend Running!");
 });
 
-// PORT
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
