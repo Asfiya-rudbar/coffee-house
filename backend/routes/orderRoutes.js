9513dcea1fd48899a0d9be0539a913dc29dbc1a6
Asfiya Rudbar <asfiyarudbar@gmail.com>
2026-05-06 17:55:25 +0500

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,7 +4,6 @@ import Product from "../models/Product.js";
 
 const router = express.Router();
 
-// GET all products
 router.get("/", async (req, res) => {
   const category = req.query.category;
   const filter = category ? { category } : {};
@@ -12,20 +11,17 @@ router.get("/", async (req, res) => {
   res.json(products);
 });
 
-// POST add new product (Admin)
 router.post("/", async (req, res) => {
   const product = new Product(req.body);
   await product.save();
   res.json(product);
 });
 
-// PUT update product (Admin)
 router.put("/:id", async (req, res) => {
   const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
   res.json(product);
 });
 
-// DELETE product (Admin)
 router.delete("/:id", async (req, res) => {
   await Product.findByIdAndDelete(req.params.id);
   res.json({ message: "Product deleted" });
