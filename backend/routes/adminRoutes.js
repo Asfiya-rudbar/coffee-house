cc6269ef693b7305eef780d216debb420e77744c
Asfiya Rudbar <asfiyarudbar@gmail.com>
2026-05-06 17:56:04 +0500

    Clean up comments in adminRoutes.js
    Removed comments for clarity in admin routes.

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -4,32 +4,27 @@ import Order from "../models/Order.js";
 
 const router = express.Router();
 
-// GET all products
 router.get("/products", async (req, res) => {
   const products = await Product.find();
   res.json(products);
 });
 
-// POST add new product
 router.post("/products", async (req, res) => {
   const product = new Product(req.body);
   await product.save();
   res.json(product);
 });
 
-// PUT update product
 router.put("/products/:id", async (req, res) => {
   const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
   res.json(product);
 });
 
-// DELETE product
 router.delete("/products/:id", async (req, res) => {
   await Product.findByIdAndDelete(req.params.id);
   res.json({ message: "Product deleted" });
 });
 
-// GET all orders
 router.get("/orders", async (req, res) => {
   const orders = await Order.find().sort({ createdAt: -1 });
   res.json(orders);
