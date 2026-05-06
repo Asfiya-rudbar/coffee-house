010938cf85aaa076f3e56d8f217a137757b4eaaf
Asfiya Rudbar <asfiyarudbar@gmail.com>
2026-05-06 17:56:33 +0500

    Refactor Product schema formatting

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  category: { type: String, required: true }, // Hot Coffee / Cold Coffee
-  image: { type: String } // URL or image path
+  category: { type: String, required: true }, 
+  image: { type: String } 
 });
 
 const Product = mongoose.model("Product", productSchema);
